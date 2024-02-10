bd8fb86e8992c04ae66905e053550455a8ce4f56
mjmt41-sorutrt <kokishiono.58@gmail.com>
2024-02-10 13:15:09 +0900

    it's commit

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
 starship init fish | source
 
 #mise (nodejsなどのバージョン管理ツール)~/.local/share/mise/bin/mise activate fish | source
-~/.local/share/mise/bin/mise activate fish | source
+~/.local/bin/mise activate fish | source
