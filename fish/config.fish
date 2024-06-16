0c25a90be85309ed8052cb7c47eb2f8e62cbb0ca
mjmt41-sorutrt <kokishiono.58@gmail.com>
2024-06-16 14:24:47 +0900

    add: opam setting

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,8 @@ starship init fish | source
 
 #mise (nodejsなどのバージョン管理ツール)~/.local/share/mise/bin/mise activate fish | source
 ~/.local/bin/mise activate fish | source
+
+# set -x PATH (npm get prefix -g)/bin $PATH
+
+# opam configuration
+source /home/sorutrt/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
