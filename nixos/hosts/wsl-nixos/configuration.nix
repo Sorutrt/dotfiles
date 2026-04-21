52c3403406e9ebdbb0e745353f8216f8a5b1813a
sorutrt <kokishiono.58@gmail.com>
2026-04-21 05:43:49 +0000

    jounald survival-mem 50MiB

--- nixos/hosts/wsl-nixos/configuration.nix
+++ nixos/hosts/wsl-nixos/configuration.nix
@@ -16,6 +16,10 @@
   wsl.enable = true;
   wsl.defaultUser = "nixos";
 
+  services.journald.extraConfig = ''
+    SystemMaxUse=50M
+  '';
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
