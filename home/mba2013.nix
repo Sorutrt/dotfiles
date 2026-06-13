15c5a15620699f8fee891fa2b64adbf768806728
sorutrt <kokishiono.58@gmail.com>
2026-06-13 23:58:03 +0900

    mozcTool を触れるようにする

--- home/mba2013.nix
+++ home/mba2013.nix
@@ -2,6 +2,9 @@
 
 let
   hyprlandConfigFile = "${config.home.homeDirectory}/dotfiles/hypr/hyprland.lua";
+  mozcTool = pkgs.writeShellScriptBin "mozc_tool" ''
+    exec ${pkgs.mozc}/lib/mozc/mozc_tool "$@"
+  '';
 
 in
 {
@@ -16,7 +19,9 @@ in
   home.packages = with pkgs; [
     brightnessctl
     discord
+    mozcTool
     playerctl
+    wl-clipboard
   ];
 
   home.file.".config/hypr/hyprland.lua" = {
