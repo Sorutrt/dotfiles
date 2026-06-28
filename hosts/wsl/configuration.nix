ea314388114ac2dbf68ec92c8ce146fed52e87a3
sorutrt <kokishiono.58@gmail.com>
2026-06-28 04:29:43 +0000

    wsl networking

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -26,5 +26,19 @@
     SystemMaxUse=50M
   '';
 
-  networking.resolvconf.enable = false;
+  # networking.resolvconf.enable = false;   wsl 側に /etc/resolv.conf を自動生成させる
+  wsl.wslConf.network.generateResolvConf = true;
+ 
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+ 
+   nix = {
+     settings = {
+       experimental-features = ["nix-command" "flakes"];
+     };
+   };
 }
