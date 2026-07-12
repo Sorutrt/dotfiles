e4ba49ce038af8dbc78cef8514df8e84c28810e3
sorutrt <kokishiono.58@gmail.com>
2026-07-12 21:13:06 +0900

    darkmode

--- home/desktop.nix
+++ home/desktop.nix
@@ -25,7 +25,8 @@ in
 
   home.file.".config/niri/config.kdl".force = true;
 
-  dconf.settings."org/gnome/desktop/interface" = {
-    color-scheme = "prefer-dark";
+  gtk = {
+    enable = true;
+    colorScheme = "dark";
   };
 }
