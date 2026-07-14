caefcfd6f35088c3e8c90f399adcc223e4dc5d3d
sorutrt <kokishiono.58@gmail.com>
2026-07-14 10:21:51 +0900

    regulaly gc in nix (weekly, older than 30days)

--- flake.nix
+++ flake.nix
@@ -26,6 +26,11 @@
       users.defaultUserShell = pkgs.nushell;
 
       nix = {
+        gc = {
+          automatic = true;
+          dates = "weekly";
+          options = "--delete-older-than 30d";
+        };
         settings = {
           experimental-features = [ "nix-command" "flakes" ];
         };
